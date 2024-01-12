6b32879d64b3329523b0bb3c02e059817fedc7c8
Ankit Das <www.das5471@gmail.com>
2024-01-13 02:53:03 +0530

    solution

--- app/page.js
+++ app/page.js
@@ -1,16 +1,26 @@
 'use client'
 import React, { useState, useEffect } from 'react';
-import CoinCard from './components/CoinCard';
+import CoinCard from '../components/CoinCard';
 
 function Home() {
     const [coins, setCoins] = useState([]);
+
+    useEffect(() => {  
+        const fetchCoins = async () => {
+            const response = await fetch('https://api.coinlore.net/api/tickers/');
+            const data = await response.json();  
+            setCoins(data.data.slice(0, 20));
+        };
+        fetchCoins();
+    
+    },[])
 
     return (
         <div className='home'>
             <h1>Top 20 Cryptos</h1>
             <div className='coins-container'>
                 {coins.map((coin) => (
-                    <CoinCard />
+                    <CoinCard coin={coin} key={coin.id} />
                 ))}
             </div>
         </div>
